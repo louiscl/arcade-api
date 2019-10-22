9b082f12d607098cbef2fdb6ce31744463a0d2d4
github username <github email address>
2019-10-22 19:27:38 +0100

    rails api

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create(player: 'michelle', score: 50)
+Game.create(player: 'louis6', score: 25)
+Game.create(player: 'louis6', score: 40)
+Game.create(player: 'max96', score: 70)
